4350bea03dd3133da7178e628ffda33d4021089c
YuriMityaev <eksia@bk.ru>
2025-01-19 22:00:16 +0500

    First commit again

---
+++ main.py
@@ -0,0 +1,26 @@
+# n = 5.5
+
+# print(type(n))
+
+
+# a = 5
+# b = 5.54
+# c = "Hello"5
+5
+# print(f"{a} - {b} - {c}")
+
+# print("{} - {} - {}".format(a,b,c))
+
+print("Input first number")
+a = input()
+
+b = input ("Please input second number:")
+
+print(a, "+", b, "=", a + b)12
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
