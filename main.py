fd2887337a8a5839c9981f40290615c855ce848b
YuriMityaev <eksia@bk.ru>
2025-01-20 23:29:49 +0500

    added seminar 1

--- main.py
+++ main.py
@@ -6,14 +6,14 @@
 # a = 5
 # b = 5.54
 # c = "Hello"5
-5
+
 # print(f"{a} - {b} - {c}")
 
 # print("{} - {} - {}".format(a,b,c))
 
 # print("Input first number")
 # a = int(input())
-# b = int(input("Please input second number:"))
+# b = int(input("Please input second numb10er:"))
 
 # print(a, "+", b, "=", a + b)
 
@@ -28,4 +28,44 @@
 # b = 5.43543545
 # print(round(a*b, 3))
 
+# Задач №1. Семинар 1. 
 
+# n = int(input("Fill first number:"))
+# m = int(input("Fill second number:"))
+
+# print((m + n - 1)//n)
+
+# Задача №2 Семинар 1.
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# print(((a + 1)//2) + ((b + 1)//2) + ((c+1)//2))
+
+# Задача 3 семинар 2.
+
+# i = int(input())
+# j = int(input())
+
+# if i - j == 0:
+#     print(-1)
+# else:
+#     print ( i+j - 1)
+
+# n = int(input())
+
+# if n % 4 == 0 and n % 100 != 0 or n % 400 == 0:
+#     print("yes")   
+# else:
+#     print("no")
+
+# HW
+
+# ex1.
+
+n = int(input("Введите трехзначное число:"))
+if 100 <= n <= 999:
+    print (n[0] + n [1] + n [2])
+else:
+    print ("Введено не трехзначное число повторите заново")
